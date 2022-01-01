4322f57819a572034f78dc85518266dcaca01b00
alasgar88 <mahmudov.alasgar@gmail.com>
2022-01-01 23:13:49 +0400

    third commit

---
+++ travel/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    name = "Alasgar"
+    return render(request,'home.html',{"name":name})
+
+
+def about(request):
+    name = "About Us"
+    return render(request,'about.html',{"name":name})
